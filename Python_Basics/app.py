d4fdf46d32dcc4996d0caa85e77eb056650b7599
Shaheer <shaheerkamran13@gmail.com>
2026-02-16 17:25:32 +0500

    first commit

---
+++ Python_Basics/app.py
@@ -0,0 +1,20 @@
+print("Hi")
+print(1+5)
+
+
+for i in range(5):
+    print(i)
+
+
+# Example - Calculate the sum of first n natural numbers using while and for loop
+
+num = int(input("Enter the numebr: "))
+counter = 1
+sum = 0 
+
+
+while counter<=num:
+    sum = sum+counter
+    count +=1
+
+print('The total sum: ', sum)
